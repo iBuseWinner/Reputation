53bd038d5b1c78902a441566d505acd18845f6d3
Sergey <busewin@vk.com>
2022-12-14 18:18:27 +0300

    Добавлен enum UpdateAction

--- src/main/java/ru/fennec/free/reputation/handlers/enums/UpdateAction.java
+++ src/main/java/ru/fennec/free/reputation/handlers/enums/UpdateAction.java
@@ -1,4 +1,4 @@
-package ru.fennec.free.reputation.common.enums;
+package ru.fennec.free.reputation.handlers.enums;
 
 public enum UpdateAction {
     INCREASE, //Когда какой-либо игрок выдаёт очко репутации другому игроку
