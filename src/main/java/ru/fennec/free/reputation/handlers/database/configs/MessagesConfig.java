c1d8a56a29252ee680838c5921f7a5c6be3805ba
Sergey <busewin@vk.com>
2022-12-25 19:13:54 +0300

    Больше сообщений!!!! И добавлена строка про /reputation reload

--- src/main/java/ru/fennec/free/reputation/handlers/database/configs/MessagesConfig.java
+++ src/main/java/ru/fennec/free/reputation/handlers/database/configs/MessagesConfig.java
@@ -30,6 +30,7 @@ public interface MessagesConfig {
 
         @ConfDefault.DefaultStrings({"${prefix} &fАктуальный список команд плагина для Администраторов:",
                 "  &a/reputation help -&f открыть данный список команд",
+                "  &a/reputation reload -&f перезагрузить конфиги плагина (не трогает БД)",
                 "  &a/reputation player <Игрок> reset -&f сбросить репутацию игроку",
                 "  &a/reputation player <Игрок> set <Очки> -&f установить репутацию игроку"})
         @ConfComments("Ответ Администратору, когда он запрашивает список команд")
@@ -47,10 +48,18 @@ public interface MessagesConfig {
 
         @ConfDefault.DefaultStrings({"${prefix} &fАктуальный список команд плагина для Игроков:",
                 "  &a/reputation help -&f открыть данный список команд",
-                "  &a/reputation player <Игрок> reset -&f сбросить репутацию игроку",
-                "  &a/reputation player <Игрок> set <Очки> -&f установить репутацию игроку"})
+                "  &a/reputation <self|me|info> -&f посмотреть информацию о себе",
+                "  &a/reputation <Игрок> -&f посмотреть информацию об онлайн игроке",
+                "  &a/reputation give <Игрок> -&f прибавить очко репутации игроку"})
         @ConfComments("Ответ Администратору, когда он запрашивает список команд")
         List<String> helpStrings();
+
+        @ConfDefault.DefaultString("${prefix} &cКоманда доступна только игрокам!")
+        @ConfComments("Сообщение, если команду использует не игрок")
+        String notAPlayer();
+
+        @ConfDefault.DefaultString("${prefix} &fУ Вас сейчас &a${reputation}&f очков репутации!")
+        String selfInfo();
     }
 
 }
