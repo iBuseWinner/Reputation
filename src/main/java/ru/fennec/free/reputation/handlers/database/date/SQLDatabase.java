1d636838c38cf260b5642a863939a43fd31dcf29
Sergey <busewin@vk.com>
2023-01-02 16:34:04 +0300

    Фиксы в sql

--- src/main/java/ru/fennec/free/reputation/handlers/database/date/SQLDatabase.java
+++ src/main/java/ru/fennec/free/reputation/handlers/database/date/SQLDatabase.java
@@ -34,27 +34,23 @@ public class SQLDatabase implements IDatabase {
     @Override
     public void initializeTables() {
         this.jdbi.useHandle(handle -> {
-            handle.execute("CREATE TABLE IF NOT EXISTS ? (" +
+            handle.execute("CREATE TABLE IF NOT EXISTS `" + this.databaseSection.tableName() + "` (" +
                             "`id` BIGINT(50) auto_increment, " +
                             "`uuid` VARCHAR(50), " +
                             "`reputation` BIGINT(50), " +
-                            "PRIMARY KEY (`id`) USING BTREE);",
-                    this.databaseSection.tableName());
-            handle.execute("CREATE TABLE IF NOT EXISTS ? (" +
+                            "PRIMARY KEY (`id`) USING BTREE);");
+            handle.execute("CREATE TABLE IF NOT EXISTS `" + this.databaseSection.favoritesTableName() + "` (" +
                             "`id` BIGINT(50), " +
-                            "`favorite` BIGINT(50), " +
-                            "USING BTREE);",
-                    this.databaseSection.favoritesTableName());
+                            "`favorite` BIGINT(50));");
         });
     }
 
     @Override
     public void insertNewPlayer(IGamePlayer gamePlayer) {
         jdbi.useHandle(handle -> {
-            handle.execute("INSERT IGNORE INTO ? " +
-                            "(`uuid`)" +
-                            "VALUES (?);",
-                    this.databaseSection.tableName(),
+            handle.execute("INSERT IGNORE INTO `" + this.databaseSection.tableName() + "` " +
+                            "(`uuid`, `reputation`)" +
+                            "VALUES (?, '0');",
                     gamePlayer.getGamePlayerUUID().toString());
         });
     }
@@ -62,19 +58,18 @@ public class SQLDatabase implements IDatabase {
     @Override
     public void savePlayer(IGamePlayer gamePlayer) {
         this.jdbi.useHandle(handle -> {
-            handle.execute("UPDATE ? SET " +
-                            "`reputation`=?",
-                    this.databaseSection.tableName(),
-                    gamePlayer.getPlayerReputation());
+            handle.execute("UPDATE `" + this.databaseSection.tableName() + "` SET " +
+                            "`reputation`=? WHERE `id`=?",
+                    gamePlayer.getPlayerReputation(),
+                    gamePlayer.getId());
         });
     }
 
     @Override
     public void saveAction(IGamePlayer acting, IGamePlayer target) {
         this.jdbi.useHandle(handle -> {
-            handle.execute("INSERT INTO ? " +
+            handle.execute("INSERT INTO `" + this.databaseSection.favoritesTableName() + "` " +
                             "(`id`, `favorite`) VALUES (?, ?);",
-                    this.databaseSection.favoritesTableName(),
                     acting.getId(),
                     target.getId());
         });
@@ -83,8 +78,7 @@ public class SQLDatabase implements IDatabase {
     @Override
     public void deleteAction(IGamePlayer gamePlayer) {
         this.jdbi.useHandle(handle -> {
-            handle.execute("DELETE FROM ? WHERE `id`=? OR `favorite`=?",
-                    this.databaseSection.favoritesTableName(),
+            handle.execute("DELETE FROM `" + this.databaseSection.favoritesTableName() + "` WHERE `id`=? OR `favorite`=?",
                     gamePlayer.getId(),
                     gamePlayer.getId());
         });
@@ -94,15 +88,13 @@ public class SQLDatabase implements IDatabase {
     public IGamePlayer wrapPlayer(Player player) {
         AtomicReference<IGamePlayer> atomicGamePlayer = new AtomicReference<>();
         this.jdbi.useHandle(handle -> {
-            IGamePlayer gamePlayer = handle.createQuery("SELECT * FROM ? WHERE `uuid`=?;")
-                    .bind(0, this.databaseSection.tableName())
-                    .bind(1, player.getUniqueId().toString())
+            IGamePlayer gamePlayer = handle.createQuery("SELECT * FROM `" + this.databaseSection.tableName() + "` WHERE `uuid`=?;")
+                    .bind(0, player.getUniqueId().toString())
                     .map(new GamePlayerMapper())
                     .first();
 
-            gamePlayer.setIDsWhomGaveReputation(handle.createQuery("SELECT * FROM ? WHERE `id`=?;")
-                    .bind(0, this.databaseSection.favoritesTableName())
-                    .bind(1, gamePlayer.getId())
+            gamePlayer.setIDsWhomGaveReputation(handle.createQuery("SELECT * FROM `" + this.databaseSection.favoritesTableName() + "` WHERE `id`=?;")
+                    .bind(0, gamePlayer.getId())
                     .mapTo(Long.class)
                     .list());
 
