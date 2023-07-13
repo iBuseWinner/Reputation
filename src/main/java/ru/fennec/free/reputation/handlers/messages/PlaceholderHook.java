dd4cb85f15d7b593b81139fade7bce970fdad920
Sergey <busewin@vk.com>
2023-07-13 22:28:00 +0300

    Добавлены цвета в заполнитель %reputation_reputation%

--- src/main/java/ru/fennec/free/reputation/handlers/messages/PlaceholderHook.java
+++ src/main/java/ru/fennec/free/reputation/handlers/messages/PlaceholderHook.java
@@ -3,10 +3,14 @@ package ru.fennec.free.reputation.handlers.messages;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import ru.fennec.free.reputation.common.configs.ConfigManager;
 import ru.fennec.free.reputation.common.interfaces.IDatabase;
 import ru.fennec.free.reputation.common.interfaces.IGamePlayer;
+import ru.fennec.free.reputation.handlers.database.configs.MainConfig;
 import ru.fennec.free.reputation.handlers.players.PlayersContainer;
 import ru.fennec.free.reputation.handlers.players.TitlesHandler;
+
+import java.util.LinkedHashMap;
 
 public class PlaceholderHook extends PlaceholderExpansion {
 
@@ -14,12 +18,15 @@ public class PlaceholderHook extends PlaceholderExpansion {
     private final PlayersContainer playersContainer;
     private final IDatabase database;
     private final TitlesHandler titlesHandler;
+    private MainConfig mainConfig;
 
-    public PlaceholderHook(String version, PlayersContainer playersContainer, IDatabase database, TitlesHandler titlesHandler) {
+    public PlaceholderHook(String version, PlayersContainer playersContainer, IDatabase database,
+                           TitlesHandler titlesHandler, ConfigManager<MainConfig> mainConfigManager) {
         this.version = version;
         this.playersContainer = playersContainer;
         this.database = database;
         this.titlesHandler = titlesHandler;
+        this.mainConfig = mainConfigManager.getConfigData();
     }
 
     @Override
@@ -48,7 +55,18 @@ public class PlaceholderHook extends PlaceholderExpansion {
         if (gamePlayer != null) {
             switch (params.toLowerCase()) {
                 case "reputation": //%reputation_reputation% - показывает игроку его репутацию
-                    return String.valueOf(gamePlayer.getPlayerReputation());
+                    String color = "";
+                    long reputation = gamePlayer.getPlayerReputation();
+                    if (mainConfig.color().enable()) {
+                        if (reputation < 0) {
+                            color = mainConfig.color().negativeReputation();
+                        } else if (reputation == 0) {
+                            color = mainConfig.color().neutralReputation();
+                        } else {
+                            color = mainConfig.color().positiveReputation();
+                        }
+                    }
+                    return color + reputation;
                 case "id": //%reputation_id% - показывает игроку его id из БД
                     return String.valueOf(gamePlayer.getId());
                 case "favorites_amount": //%reputation_favorites_amount% - показывает, скольким игрокам игрок давал очко репутации
@@ -86,10 +104,15 @@ public class PlaceholderHook extends PlaceholderExpansion {
                                 return String.valueOf(database.getTopGamePlayerReputationByReputation(place));
                             }
                         }
-                    } catch (NumberFormatException ignored) {  }
+                    } catch (NumberFormatException ignored) {
+                    }
                     return params;
             }
         }
         return params;
+    }
+
+    public void updateConfigData(ConfigManager<MainConfig> mainConfigManager) {
+        this.mainConfig = mainConfigManager.getConfigData();
     }
 }
