aa6b28af50b917b303a0ecb157d01f7615d16cdd
Sergey <busewin@vk.com>
2024-08-11 14:53:17 +0300

    Изменена система топа, добавлен кэш топа, который обновляется раз в N тиков (это настраивается в конфиге, но применяется только после рестарта сервера)

--- src/main/java/ru/fennec/free/reputation/handlers/players/PlayersContainer.java
+++ src/main/java/ru/fennec/free/reputation/handlers/players/PlayersContainer.java
@@ -1,20 +1,33 @@
 package ru.fennec.free.reputation.handlers.players;
 
+import org.bukkit.Bukkit;
+import ru.fennec.free.reputation.ReputationPlugin;
+import ru.fennec.free.reputation.common.configs.ConfigManager;
+import ru.fennec.free.reputation.common.interfaces.IDatabase;
 import ru.fennec.free.reputation.common.interfaces.IGamePlayer;
+import ru.fennec.free.reputation.handlers.database.configs.MainConfig;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class PlayersContainer {
 
+    private ReputationPlugin plugin;
+    private IDatabase database;
+    private MainConfig mainConfig;
+
+    //Список кэш игроков в топе по репутации
+    private Map<String, Long> cachedTopGamePlayers;
     //Список кэш игроков
     private final List<IGamePlayer> cachedPlayers;
 
-    public PlayersContainer() {
+    public PlayersContainer(ReputationPlugin plugin, IDatabase database, ConfigManager<MainConfig> mainConfigManager) {
+        this.plugin = plugin;
+        this.database = database;
+        this.mainConfig = mainConfigManager.getConfigData();
         this.cachedPlayers = new ArrayList<>();
+        this.cachedTopGamePlayers = new LinkedHashMap<>();
+        updateCachedTopPlayers();
     }
 
     /***
@@ -61,7 +74,7 @@ public class PlayersContainer {
     }
 
     /***
-     * Получить топ-N игрока по репутации
+     * Получить топ-N онлайн игрока по репутации
      *
      * @param place N - место игрока в топе (начинается с нуля)
      * @return IGamePlayer - игрока на N месте. Ошибка стоп ноль ноль ноль ноль если нет такого
@@ -69,6 +82,9 @@ public class PlayersContainer {
     public IGamePlayer getTopGamePlayerByReputation(int place) {
 //        return cachedPlayers //Тут ошибка в методе ору, возвращается всегда топ с первого места :/
 //                .stream().max(Comparator.comparingLong(IGamePlayer::getPlayerReputation)).get();
+        if (place >= cachedPlayers.size()) {
+            return null;
+        }
         return cachedPlayers
                 .stream()
 //                .sorted((f1, f2) -> Long.compare(f2.getPlayerReputation(), f1.getPlayerReputation()))
@@ -76,4 +92,50 @@ public class PlayersContainer {
                 .collect(Collectors.toList())
                 .get(place);
     }
+
+    /**
+     * Список обновляется каждые N тиков (настраивается в конфиге, параметр topUpdateFrequency)
+     * @return Map<String,Long> - список топ игроков по репутации (String - ник, Long - очки репутации)
+     */
+    public Map<String, Long> getCachedTopGamePlayers() {
+        return cachedTopGamePlayers;
+    }
+
+    private void updateCachedTopPlayers() {
+        Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, () -> {
+            cachedTopGamePlayers.clear();
+            Map<String, Long> mergedTopPlayers = new HashMap<>();
+
+            for (int i = 0; i < mainConfig.topAmount(); i++) {
+                IGamePlayer onlinePlayer = getTopGamePlayerByReputation(i);
+                if (onlinePlayer != null) {
+                    mergedTopPlayers.put(onlinePlayer.getBukkitPlayer().getName(), onlinePlayer.getPlayerReputation());
+                }
+            }
+
+            for (int i = 0; i < mainConfig.topAmount(); i++) {
+                String playerName = Bukkit.getOfflinePlayer(database.getTopGamePlayerUUIDByReputation(i+1)).getName();
+                long playerReputation = database.getTopGamePlayerReputationByReputation(i+1);
+                if (!mergedTopPlayers.containsKey(playerName)) {
+                    mergedTopPlayers.put(playerName, playerReputation);
+                }
+            }
+
+            cachedTopGamePlayers = mergedTopPlayers.entrySet()
+                    .stream()
+                    .sorted((entry1, entry2) -> Long.compare(entry2.getValue(), entry1.getValue()))
+                    .limit(mainConfig.topAmount())
+                    .collect(Collectors.toMap(
+                            Map.Entry::getKey,
+                            Map.Entry::getValue,
+                            (e1, e2) -> e1,
+                            LinkedHashMap::new
+                    ));
+        }, 0, mainConfig.topUpdateFrequency());
+    }
+
+    public void updateConfigData(ConfigManager<MainConfig> mainConfigManager) {
+        this.mainConfig = mainConfigManager.getConfigData();
+    }
+
 }
